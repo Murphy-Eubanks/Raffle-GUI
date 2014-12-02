e14d18b352207b44b4b790282d05f8e46fac7eb7
Shawn Eubanks <dovahdovolom@gmail.com>
2014-12-02 09:20:59 -0600

    :/

--- gui.py
+++ gui.py
@@ -5,15 +5,4 @@ from tkinter import ttk
 root = Tk()
 root.title("Raffle")
 
-mainframe = ttk.Frame(root, padding='3 3 10 10')
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-def grid():
-	gnum = ttk.Entry(mainframe, width=5)
-	
-
-
-
 tk.mainloop()
